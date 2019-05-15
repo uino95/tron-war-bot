9cad58f152f157aa547b9029035da57747e07fc6
Matteo Sinico <sinicomatteo@gmail.com>
2019-05-15 23:29:39 +0200

    solved issue on sagas with import

--- src/sagas/handleWebSocket.js
+++ src/sagas/handleWebSocket.js
@@ -2,15 +2,15 @@ import { all, call, put, spawn, takeEvery, select } from "redux-saga/effects";
 import socketIOClient from "socket.io-client";
 
 function handleEvent(data){
-
+	console.log(data)
 }
 
 function* handleWebSocket() {
-	const endpoint = "";
+	const endpoint = "http://localhost:4000";
 
 	const socket = yield call(socketIOClient, endpoint);
 
-    socket.on("nomeEvento", data => handleEvent(data));
+    socket.on("sendSocketid", data => handleEvent(data));
   
 
   yield all([
@@ -18,4 +18,4 @@ function* handleWebSocket() {
   ]);
 }
 
-export default handleRequestSaga;
+export default handleWebSocket;
