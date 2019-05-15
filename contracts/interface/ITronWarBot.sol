a4bf9858d1d1356784d40b39a47b84ae40818a02
Sam <samuele.rodi@pwc.com>
2019-05-15 11:53:55 +0200

    Frontend interface edit

--- contracts/interface/ITronWarBot.sol
+++ contracts/interface/ITronWarBot.sol
@@ -5,16 +5,20 @@ import '../interface/IWarCoin.sol';
 interface ITronWarBot {
 
   /* TronWarBot */
-  /* Return current House Edge in percentage: 1 TRX equals 100% */
+  /* Returns current House Edge in percentage: 1 TRX equals 100% */
   function houseEdge() external view returns (uint256);
+  /* Returns currently set address of house */
+  function houseAddress() external returns (address);
   /* Return minimum and maximum bet set for specific game type */
   function gameParams(uint256 _gameType) external view returns (uint256, uint256);
   /* Set new house edge: 1 TRX equals 100% */
   function setHouseEdge(uint256 _houseEdge) external returns (bool);
+  /* Set the payable address of the house */
+  function setHouseAddress(address payable _house) external returns (bool);
   /* Set the specific game params:  minimum bet and maximum bet expressed in SUN */
   function setGameParams(uint256 _gameType, uint256 _minimumBet, uint256 _maximumBet) external returns (bool);
   /* It places the bet, it mints x token for the user and x for the house and records it in an event */
-  function bet(uint256 _gameType, uint256 _userChoice) external returns (bool);
+  function bet(uint256 _gameType, uint256 _userChoice) external payable returns (bool);
   /* It pays the user after deducting the house edge and send TRX to house and to user */
   function payout(address _recipient, uint256 _amount) external returns (bool);
 
