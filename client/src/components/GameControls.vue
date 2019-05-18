1432cec25ec4b8434cb2077a0786e3ba97108a8e
Andrea Taglia <andrea.taglia@pwc.com>
2019-05-18 19:22:33 +0200

    modified project structure and switched from reactjs to vue.js for the frontend

---
+++ client/src/components/GameControls.vue
@@ -0,0 +1,52 @@
+<template>
+<v-tabs centered color="grey lighten-1" dark icons-and-text>
+  <v-tabs-slider color="red lighten-4"></v-tabs-slider>
+
+  <v-tab href="#tab-1">
+    Recents
+    <v-icon>phone</v-icon>
+  </v-tab>
+
+  <v-tab href="#tab-2">
+    Favorites
+    <v-icon>favorite</v-icon>
+  </v-tab>
+
+  <v-tab href="#tab-3">
+    Nearby
+    <v-icon>account_box</v-icon>
+  </v-tab>
+
+  <v-tab-item v-for="i in 3" :key="i" :value="'tab-' + i">
+    <v-card flat>
+      <v-card-text>{{ text }}</v-card-text>
+    </v-card>
+  </v-tab-item>
+  
+</v-tabs>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    text: "ciaooooooooooo nsakfdjed skndlej deandnlnd jdnew",
+    ecosystem: [{
+        text: 'vuetify-loader',
+        href: 'https://github.com/vuetifyjs/vuetify-loader'
+      },
+      {
+        text: 'github',
+        href: 'https://github.com/vuetifyjs/vuetify'
+      },
+      {
+        text: 'awesome-vuetify',
+        href: 'https://github.com/vuetifyjs/awesome-vuetify'
+      }
+    ]
+  })
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/src/components/GameMap.vue
@@ -0,0 +1,60 @@
+<template>
+<v-container flex>
+  <div id="datamap"></div>
+</v-container>
+</template>
+
+
+<script>
+import * as Datamap from "datamaps";
+import * as d3 from "d3";
+export default {
+  props: {
+    projects: Array,
+    name: String
+  },
+  data: () => ({
+    width: 0,
+    height: 0
+  }),
+  computed: {},
+  mounted() {
+    var map = new Datamap({
+      element: document.getElementById('datamap'),
+      fills: {
+        HIGH: '#afafaf',
+        LOW: '#123456',
+        MEDIUM: 'blue',
+        UNKNOWN: 'rgb(0,0,0)',
+        defaultFill: 'green'
+      },
+      data: {
+        IRL: {
+          fillKey: 'LOW',
+          numberOfThings: 2002
+        },
+        USA: {
+          fillKey: 'MEDIUM',
+          numberOfThings: 10381
+        }
+      }
+    });
+
+    // Pure JavaScript
+   window.addEventListener('resize', function() {
+       map.resize();
+   });
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<style scoped>
+#datamap {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+</style>
