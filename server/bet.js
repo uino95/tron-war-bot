79b56e55c662a173681e56fe13365372bf975107
Matteo Sinico <sinicomatteo@gmail.com>
2019-07-24 16:25:16 +0200

    added bet.js

--- server/bet.js
+++ server/bet.js
@@ -5,13 +5,9 @@ const tronWeb = require('tronweb')
 
 const validateFullRunWinner = (b) => {
   // @TODO: Add bet price formula
-<<<<<<< HEAD
-  return tronWeb.fromSun(b.amount.toString()) == '1';
-=======
   if (!b || !b.amount) return false;
   var fixedAmount = config.test ? "1" : "50";
   return b.amount.eq(tronWeb.toSun(fixedAmount));
->>>>>>> 9fedb12d8d40bb431267e922a86d946a0eb32e2f
 }
 
 const validateNextConquerer = (b) => {
