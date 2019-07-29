820ece2a3a54f40cb25ca4cac66e800af49e1f61
michelepapale1996 <michele.papale@mail.polimi.it>
2019-07-29 17:23:55 +0200

    Minor fixes
    - background setted to grey
    - fixed bet UX when user rejects the transaction
    - added pagination to all the lists
    - truncated to 20 the latestBets components
    - fixed color of stats component

--- client/src/components/GameControls.vue
+++ client/src/components/GameControls.vue
@@ -4,13 +4,13 @@
     <v-spacer></v-spacer>
     <v-toolbar-items>
       <v-btn flat replace to="/betfinal">
-        <v-icon>public</v-icon> Bet Final
+        <v-icon color="primary_final_tab">public</v-icon> Bet Final
       </v-btn>
       <v-btn flat replace to="/betnext">
-        <v-icon>attach_money</v-icon> Bet Next
+        <v-icon color="primary_next_tab">attach_money</v-icon> Bet Next
       </v-btn>
       <v-btn flat replace to="/stats">
-        <v-icon>poll</v-icon> Stats
+        <v-icon color="primary_stats_tab">poll</v-icon> Stats
       </v-btn>
     </v-toolbar-items>
     <v-spacer></v-spacer>
