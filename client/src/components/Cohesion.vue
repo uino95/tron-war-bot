0a3446c8af5a4ff4bc8310acbf63a0230336db09
Matteo Sinico <sinicomatteo@gmail.com>
2019-09-22 18:25:23 +0200

    edit date in cohesion tab

--- client/src/components/Cohesion.vue
+++ client/src/components/Cohesion.vue
@@ -157,10 +157,10 @@
         var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
         var year = a.getFullYear();
         var month = months[a.getMonth()];
-        var day = a.getDate();
-        var hour = a.getHours();
-        var min = a.getMinutes();
-        var sec = a.getSeconds();
+        var day = a.getDate() < 10 ? '0' + a.getDate() : a.getDate();
+        var hour = a.getHours() < 10 ? '0' + a.getHours() : a.getHours();
+        var min = a.getMinutes() < 10 ? '0' + a.getMinutes() : a.getMinutes();
+        var sec = a.getSeconds() < 10 ? '0' + a.getSeconds() : a.getSeconds();
         var time = day + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
         return time;
       }
