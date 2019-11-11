4c021fe9b5c485498b97d192c825fb594074763f
Matteo Sinico <sinicomatteo@gmail.com>
2019-11-11 20:33:02 +0100

    fixed subscription to db in Game map

--- client/src/components/GameMap.vue
+++ client/src/components/GameMap.vue
@@ -25,9 +25,6 @@
   useTheme(am4themes_spiritedaway);
   export default {
     data: () => ({
-      width: 0,
-      height: 0,
-      lastSelected: null,
       polygonSeries: null,
       countriesData: null,
       chart: null,
@@ -40,38 +37,23 @@
     mounted() {
       db.ref('public/countriesMap').on('child_changed', (snapshot) => {
         let data = snapshot.val();
-        if (data.length == 241) {
-          // console.log(data)
-          data.map((el, index) => {
-            // var h = Math.floor(Math.floor((index / 10)) * (360 / 25)) / 360
-            // var s = (20 + (index % 10) * (80 / 10)) / 100
-            // var l = 0.2 + el['cohesion'] * 0.7
-            // el['color'] = this.rgbToHex(this.hslToRgb(h, s, l))
-            el['id'] = this.universalMap(index, 'charId')
-            el['cohesion'] = (el['cohesion'] * 100).toFixed(2);
-          })
-          data.map(el => {
-            el['controllerCohesion'] = (data[el['occupiedBy']]['cohesion']);
-            el['color'] = this.colorsDefault[el['occupiedBy']];
-            el['occupiedBy'] = this.universalMap(el['occupiedBy'])
-          })
-          this.polygonSeries.data = data
-          //this.polygonSeries.invalidateData()
-        }
+        data['id'] = this.universalMap(data.idx, 'charId')
+        data.cohesion = (data.cohesion * 100).toFixed(2);
+        data['controllerCohesion'] = (this.countriesData[data.occupiedBy].cohesion);
+        data['color'] = this.colorsDefault[data.occupiedBy];
+        data.occupiedBy = this.universalMap(data.occupiedBy)
+        this.polygonSeries.data[data.idx] = data
+        this.polygonSeries.invalidateData()
+
       })
 
       db.ref('public/countriesMap').once('value', (snapshot) => {
         let j = this.colors.length
         let data = snapshot.val();
         data.map((el, index) => {
-          // var h = Math.floor(Math.floor((index / 10)) * (360 / 25)) / 360
-          // var s = (20 + (index % 10) * (80 / 10)) / 100
-          // var l = 0.2 + el['cohesion'] * 0.7
-          //el['color'] = this.rgbToHex(this.hslToRgb(h, s, l))
           this.colorsDefault.push(this.colors[j])
           el['id'] = this.universalMap(index, 'charId')
           el['cohesion'] = (el['cohesion'] * 100).toFixed(2);
-          el['color'] = this.colors[j];
           j--;
           if (j < 0) {
             j = this.colors.length - 1
@@ -79,11 +61,10 @@
         })
         data.map(el => {
           el['controllerCohesion'] = (data[el['occupiedBy']]['cohesion']);
-          // el['color'] = this.colorsDefault[el['occupiedBy']];
+          el['color'] = this.colorsDefault[el['occupiedBy']];
           el['occupiedBy'] = this.universalMap(el['occupiedBy'])
         })
         this.countriesData = data
-        //this.polygonSeries.invalidateData()
         this.loadChart();
       })
     },
@@ -93,84 +74,6 @@
           'numberId'))
       },
 
-      componentToHex(c) {
-        var hex = c.toString(16);
-        return hex.length == 1 ? "0" + hex : hex;
-      },
-
-      rgbToHex(color) {
-        return "#" + this.componentToHex(color[0]) + this.componentToHex(color[1]) + this.componentToHex(color[
-          2]);
-      },
-
-      hexToRgb(hex) {
-        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-        return result ? {
-          r: parseInt(result[1], 16),
-          g: parseInt(result[2], 16),
-          b: parseInt(result[3], 16)
-        } : null;
-      },
-
-      hexToHsl(hex) {
-        var result = this.hexToRgb(hex);
-        var r = result.r;
-        var g = result.g;
-        var b = result.b
-        r /= 255, g /= 255, b /= 255;
-        var max = Math.max(r, g, b),
-          min = Math.min(r, g, b);
-        var h, s, l = (max + min) / 2;
-
-        if (max == min) {
-          h = s = 0;
-        } else {
-          var d = max - min;
-          s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-
-          switch (max) {
-            case r:
-              h = (g - b) / d + (g < b ? 6 : 0);
-              break;
-            case g:
-              h = (b - r) / d + 2;
-              break;
-            case b:
-              h = (r - g) / d + 4;
-              break;
-          }
-
-          h /= 6;
-        }
-
-        return [((h * 100 + 0.5) | 0), ((s * 100 + 0.5) | 0), ((l * 100 + 0.5) | 0)];
-      },
-
-
-      hslToRgb(h, s, l) {
-        var r, g, b;
-
-        if (s == 0) {
-          r = g = b = l; // achromatic
-        } else {
-          var hue2rgb = function hue2rgb(p, q, t) {
-            if (t < 0) t += 1;
-            if (t > 1) t -= 1;
-            if (t < 1 / 6) return p + (q - p) * 6 * t;
-            if (t < 1 / 2) return q;
-            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-            return p;
-          }
-
-          var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-          var p = 2 * l - q;
-          r = hue2rgb(p, q, h + 1 / 3);
-          g = hue2rgb(p, q, h);
-          b = hue2rgb(p, q, h - 1 / 3);
-        }
-
-        return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-      },
       loadChart() {
         /* Create map instance */
         var chart = create("chartdiv", MapChart);
@@ -193,7 +96,6 @@
 
         polygonSeries.data = this.countriesData
         this.polygonSeries = polygonSeries
-
         /* Configure series */
         var polygonTemplate = polygonSeries.mapPolygons.template;
 
@@ -207,7 +109,6 @@
         polygonTemplate.strokeOpacity = 0.5;
 
         polygonTemplate.propertyFields.fill = "color";
-
 
         polygonTemplate.events.on("hit", this.clicked)
 
@@ -241,17 +142,6 @@
             polygon.setState("default");
           });
         }
-
-        // Hide Antarctica and all the states not present in the territories from michael API
-        //polygonSeries.exclude = ["AQ"];
-
-
-        // Small map
-        // chart.smallMap = new SmallMap();
-        // // Re-position to top right (it defaults to bottom left)
-        // chart.smallMap.align = "right";
-        // chart.smallMap.valign = "top";
-        // chart.smallMap.series.push(polygonSeries);
 
         // Zoom control
         chart.zoomControl = new ZoomControl();
