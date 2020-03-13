f4395a97e8e9d31527140dad8877411e25063bba
Sam <srodi@deloitte.ie>
2020-03-13 23:25:27 +0000

    cool engine fixxx

--- server/src/social/index.js
+++ server/src/social/index.js
@@ -12,11 +12,11 @@ module.exports.runUpdate = async (cmap, td) => {
   try {
     // if (wwb.winner()) return updates.endWar();
     // if (td.turn == 1) await updates.startWar(td);
-    if (!(td.turn % 10)) await map.takeScreenshot();
-    if (!(td.turn % config.social.updates.statsFreq)) await updates.stats(td).catch(console.error);
+    // if (!(td.turn % 10)) await map.takeScreenshot();
+    // if (!(td.turn % config.social.updates.statsFreq)) await updates.stats(td).catch(console.error);
     // if (!(td.turn % config.social.updates.quotesFreq)) await updates.quotes(td);
     // if (!(td.turn % config.social.promotions.ambassadorFreq)) await promotions.becomeAmbassador(cmap, td);
-    if (!(td.turn % config.social.updates.rouletteFreq)) russianRoulette.next(cmap, td);
+    // if (!(td.turn % config.social.updates.rouletteFreq)) russianRoulette.next(cmap, td);
     // updates.battleUpdate(cmap, td);
   } catch (e) {
     console.error(e)
