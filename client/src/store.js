e68dc632352018803349d134431f1d7ddac103af
Matteo Sinico <sinicomatteo@gmail.com>
2020-03-20 12:51:30 +0100

    switch to prod

--- client/src/store.js
+++ client/src/store.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export const test = true; //REMEBER TO SWITCH TO FALSE ALSO in public/login.html
+export const test = false; //REMEBER TO SWITCH TO FALSE ALSO in public/login.html
 
 export default new Vuex.Store({
   state: {
